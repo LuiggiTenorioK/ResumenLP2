2a6addb4c6f2f6c59b91b08c0f1ab9c94065c2c1
Alvaro Lira Palomino <38359627+alvarolirap@users.noreply.github.com>
2018-06-30 21:25:41 -0500

    Ejemplo C#

---
+++ Web Services/C#/Client/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            servicioServer.WebServiceSoapClient acceso = new servicioServer.WebServiceSoapClient();
+            double suma = acceso.sumar(2.0, 3.0);
+            double resta = acceso.restar(2.5, 2.0);
+            double multiplicacion = acceso.multiplicar(2.0, 5.0);
+            double division = acceso.dividir(10.0, 2.0);
+            String saludo = acceso.saludar("Alvaro");
+            System.Console.WriteLine(suma);
+            System.Console.WriteLine(resta);
+            System.Console.WriteLine(multiplicacion);
+            System.Console.WriteLine(division);
+            System.Console.WriteLine(saludo);
+            System.Console.Read();
+        }
+    }
+}
---
+++ Web Services/C#/Server/Server/WebService.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Server
+{
+    /// <summary>
+    /// Descripción breve de WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod] public double sumar(double a, double b) { return a + b; }
+        [WebMethod] public double restar(double a, double b) { return a - b; }
+        [WebMethod] public double multiplicar(double a, double b) { return a * b; }
+        [WebMethod] public double dividir(double a, double b) { return a / b; }
+        [WebMethod] public String saludar(String nombre) { return "Hola " + nombre; }
+
+    }
+}
